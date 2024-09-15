0f4b4625e3f547cb9afc285b7e460073eef621bd
Naimur Rahman Dorjoy <nrdurjoy10oct@gmail.com>
2024-09-15 20:01:05 +0600

    After task 9

---
+++ Task 1/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt('Enter first name : ');
+      var lastName = prompt('Enter last name : ');
+        var fullName = firstName + " " + lastName + "<br/>";
+        document.write(fullName);
+        var len = fullName.length;
+        document.write("Total length : " + len + "<br/>");
+        var nameUpperCase = fullName.toUpperCase();
+        document.write(nameUpperCase);
