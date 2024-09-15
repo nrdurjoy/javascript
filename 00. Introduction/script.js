9dfe387174025c80768fed730dd69d08732f5fbc
Naimur Rahman Dorjoy <nrdurjoy10oct@gmail.com>
2024-09-15 21:35:24 +0600

    Rename & Update files

---
+++ 00. Introduction/script.js
@@ -0,0 +1,23 @@
+//Introduction of Javascript
+// JavaScript is a programming language that adds interactivity to your website. This happens in games, in the behavior of responses when buttons are pressed or with data entry on forms; with dynamic styling; with animation, etc.
+
+//The Version of Javascript
+/* The Original JavaScript ES1 ES2 ES3 (1997-1999)
+   The First Main Revision ES5 (2009)
+   The Second Revision ES6 (2015)
+   The Yearly Additions (2016, 2017 ... 2021, 2022) */
+
+//Showing Outputs
+    document.write("Hi. I'm Naimur Rahman Dorjoy");
+    alert("Hello World");
+    console.log('Let be a coder');
+
+//Date Object
+let date = new Date();console.log(date);
+let year = date.getFullYear();console.log(year);
+let month = date.getMonth();console.log(month);
+let days = date.getDate();console.log(days);
+let day = date.getDay();console.log(day);
+let hour = date.getHours();console.log(hour);
+let mins = date.getMinutes();console.log(mins);
+let sec = date.getSeconds(); console.log(sec);
